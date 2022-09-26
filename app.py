86bdc873c9c3d14b14d2cb8d0d0a3f57230b5462
Kayo Macedo <57056101+kayomacedo@users.noreply.github.com>
2022-09-26 00:51:16 -0300

    menu responsivo em css + js

--- app.py
+++ app.py
@@ -5,10 +5,16 @@ app = Flask(__name__)
 
 
 
-@app.route('/')
+@app.route('/') #Rota principal
 
 def index():
     return render_template('index.html')
+
+
+@app.route("/contatos")
+
+def pagContatos():
+    return render_template("contatos.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
